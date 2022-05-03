4a979eb9343f38811a7a1585e1941c22873dd821
Nathan <Nathan713@gmail.com>
2022-05-03 03:02:22 -0400

    Connect Home to Database

--- app/Components/ProductItem/index.js
+++ app/Components/ProductItem/index.js
@@ -7,45 +7,30 @@ import { NavigationEvents } from 'react-navigation';
 
 
 function ProductItem(props) {
-    const{title} = props;
     const{image} = props;
     const{price} = props;
-    const{ratings} = props;
-    const{oldPrice} = props;
-    const{avgRating} = props;
-    const{id} = props;
+    const{productName} = props;
+
+    //console.log(productName);
+    //console.log(price);
+    //console.log(image);
 
     const navigation = useNavigation();
     const onPress = () => {
-        navigation.navigate('ProductDetails', {id:id});
+        //navigation.navigate('ProductDetails', {id:id});
     }
     return (
     <Pressable onPress={onPress} style={styles.root}>
-        <Image 
+        { <Image 
             style={styles.image} 
             source={{uri:image}}
-         />
+         /> }
 	    <View style = {styles.rightContainer}>
-		    <Text style = {styles.title} numberOfLines={3}>{title}
+		    <Text style = {styles.title} numberOfLines={3}>{productName}
+                
             </Text>
-		    {/*Ratings*/}
-		    <View style={styles.ratingsContainer}>
-                {[0,0,0,0,0].map((el,i) =>
-                     <FontAwesome
-                    key={`${id}--${i}`}
-                     style={styles.star} 
-                     name={i < Math.floor(avgRating) ? 'star' : 'star-o'}
-                     size={18} 
-                     color={"#e47911"} 
-                     />
-                )}
-                <Text>{ratings}</Text>
-            </View>
 		    <Text style = {styles.price}>
-                from ${price}
-                {oldPrice && (
-                    <Text style={styles.oldPrice}>  was ${oldPrice}</Text>
-                )}
+                {price}
 		    </Text>
 	    </View>
     </Pressable>
--- app/Screens/HomeScreen/Home.js
+++ app/Screens/HomeScreen/Home.js
@@ -5,27 +5,58 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import ProductItem from '../../Components/ProductItem';
 import products from '../../assets/data/products';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState,useEffect } from 'react';
 
 function Home({searchValue}) {
   //const {navigation} = props;
-  
- 
+  const [JSON_DATA, setJSON_DATA] = useState('');
+
+  const onHome = async (data) => {
+    try
+    {
+      //console.log("Hello");
+      var search = '';
+      var obj = {"search":search};
+      var send = JSON.stringify(obj);
+      //console.log(send);
+      const response = await fetch('http://marketsquare.herokuapp.com/api/search',
+        {method:'POST',body:send,headers:{'Content-Type': 'application/json'}});
+      var res = JSON.parse(await response.text());
+      setJSON_DATA(res.res);
+     console.log(JSON_DATA);
+      if (res.length <= 0) {
+      }
+      else
+      {
+        console.log("Succesfull");
+      }
+    }
+    catch(e)
+    {
+      console.log("failed");
+    }
+  };
+
+  useEffect(() => {
+    onHome();
+    }, [])
+
   return (
-   
     <View style = {styles.page}>
-      <FlatList
-        data={products}
+    
+      { <FlatList
+        data={JSON_DATA}
         renderItem={({item}) => <ProductItem
-        id = {item.id} 
-        title = {item.title}
-        image = {item.image}
-        price = {item.price}
-        ratings = {item.ratings}
-        oldPrice = {item.oldPrice}
-        avgRating = {item.avgRating}
-         />}
+        productName = {item.ProductName}
+        price = {item.ProductPrice}
+        image = {item.ProductImages[0]}
+        
+         />
+        }
          showsVerticalScrollIndicator={false}
-      />
+      /> }
+     
      </View>
   );
 }
