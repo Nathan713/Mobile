c9950503688d61be01e90dd6716e3294306c52cf
J-VOL <jovanvolny@gmail.com>
2022-05-03 15:22:44 -0400

    trying images

--- app/Screens/PostScreen/index.js
+++ app/Screens/PostScreen/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ScrollView,StatusBar,Text, View, StyleSheet, TextInput, Button, Alert } from 'react-native';
+import { ScrollView, StatusBar, Text, View, StyleSheet, TextInput, Button, Alert } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
 import TakePhoto from '../../Components/TakePhoto';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -7,93 +7,120 @@ import * as yup from 'yup';
 import CustomButton from '../../Components/Button';
 import Header from '../../Components/Header';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import storage from '../firebase.js';
+
+
 
 const PostScreen = () => {
-  const[image,setImage] = useState('');
-  const [firstName,setFirstName] = useState('');
-  const [lastName,setLastName] = useState('');
-  const [phoneNumber,setPhoneNumber] = useState('');
-  const [email,setEmail] = useState('');
-  const [JWTToken,setJWTToken] = useState('');
+  const [image, setImage] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [JWTToken, setJWTToken] = useState('');
 
-    const req = "Required field"
-    const productSchema = yup.object().shape({
-        productName: yup.string().required(req),
-        productCategory: yup.string().required(req),
-        productCondition: yup.string()
-            .oneOf(["New", "Like New", "Good", "Fair", "Poor"])
-            .required(req),
-        productDescription: yup.string().required(req),
-        city: yup.string().required(req),
-        state: yup.string().required(req),
-        productPrice: yup.number().positive().integer().required(req),
-       // images: yup.mixed().nullable().test("type", "Must be a jpeg, jpg, or png", (value) => checkIfFilesAreCorrectType(value))
-           // .required(req)
-        // contactInfo: yup.string().required(), 
-        // email: yup.string().required()
-    });
+  const req = "Required field"
+  const productSchema = yup.object().shape({
+    productName: yup.string().required(req),
+    productCategory: yup.string().required(req),
+    productCondition: yup.string()
+      .oneOf(["New", "Like New", "Good", "Fair", "Poor"])
+      .required(req),
+    productDescription: yup.string().required(req),
+    city: yup.string().required(req),
+    state: yup.string().required(req),
+    productPrice: yup.number().positive().integer().required(req),
+    // images: yup.mixed().nullable().test("type", "Must be a jpeg, jpg, or png", (value) => checkIfFilesAreCorrectType(value))
+    // .required(req)
+    // contactInfo: yup.string().required(), 
+    // email: yup.string().required()
+  });
 
-    const getUserData = async() => {
-    
-      try{
-          const userData = await AsyncStorage.getItem('@storage_Key');
-          const fn = JSON.parse(userData).fn;
-          const ln = JSON.parse(userData).ln;
-          const pn = JSON.parse(userData).pn;
-          const token = JSON.parse(userData).accessToken;
-          const email = JSON.parse(userData).email;
-          setFirstName(fn);
-          setLastName(ln);
-          setEmail(email)
-          setJWTToken(token);;
-          setPhoneNumber(pn);
-          
-      }catch (e) {
-  
-      }
+  const getUserData = async () => {
+
+    try {
+      const userData = await AsyncStorage.getItem('@storage_Key');
+      const fn = JSON.parse(userData).fn;
+      const ln = JSON.parse(userData).ln;
+      const pn = JSON.parse(userData).pn;
+      const token = JSON.parse(userData).accessToken;
+      const email = JSON.parse(userData).email;
+      setFirstName(fn);
+      setLastName(ln);
+      setEmail(email)
+      setJWTToken(token);;
+      setPhoneNumber(pn);
+
+    } catch (e) {
+
     }
-    useEffect(() => {
-      getUserData();
-      }, [])
+  }
+  useEffect(() => {
+    getUserData();
+  }, [])
 
   const { register, setValue, handleSubmit, control, reset, formState: { errors } } = useForm({
     resolver: yupResolver(productSchema),
     mode: "onChange",
     reValidateMode: "all",
     defaultValues: {
-      
+
     }
   });
   const onSubmit = async (data) => {
-    try
-    {
-      const oldData =  { email: email, contactInfo: phoneNumber }
-     // console.log("Hello");
+    try {
+      const oldData = { email: email, contactInfo: phoneNumber }
+      // console.log("Hello");
+      const storeImage = async (image) => {//(image) => { 
+        return new Promise((resolve, reject) => {
+          // const fbStorage = getStorage()
+          const fileName = `${image.name}-${uuid()}`
+          const storageRef = ref(storage, '/images/' + fileName)
+
+          const uploadTask = uploadBytesResumable(storageRef, image)
+          uploadTask.on(
+            /**/
+            'state_changed',
+            (snapshot) => { },
+            (error) => {
+              reject(error)
+            },
+            () => {
+              // Handle successful uploads on complete
+              // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                resolve(downloadURL)
+              })
+            }
+          )
+        })
+      }
+      // console.log("Onsubmit: ", data)
+      const imgUrls = await Promise.all(image.map((image) => storeImage(image))).catch(() => { return })
       var send = {
         ...data,
         ...oldData,
-        ProductImages: image,
-        jwtToken: JWTToken}
+        ProductImages: imgUrls,
+        jwtToken: JWTToken
+      }
 
       var stringy = JSON.stringify(send);
       console.log(send);
       const response = await fetch('http://marketsquare.herokuapp.com/api/addproduct',
-        {method:'POST',body:stringy,headers:{'Content-Type': 'application/json'}});
+        { method: 'POST', body: stringy, headers: { 'Content-Type': 'application/json' } });
       var res = JSON.parse(await response.text());
       //console.log(res);
       if (res.length <= 0) {
         console.log("failed");
       }
-      else
-      {
+      else {
         console.log("Succesfull");
         //navigation.navigate('');
       }
     }
-    catch(e)
-    {
-      
+    catch (e) {
+
     }
   };
 
@@ -103,150 +130,150 @@ const PostScreen = () => {
     console.log(image);
   }
   return (
-    <View style ={{paddingTop: StatusBar.currentHeight,}}>
-      <Header text={"Post Item"}/>
-    
-    <ScrollView style = {{paddingBottom:550}}>
-    <View style={styles.container}>
-      
-        <TakePhoto setStateOfParent = {setStateOfParent}/>
-      <Text style={styles.label}>Product Name</Text>
-      <Controller
-        control={control}
-        render={({field: { onChange, onBlur, value }}) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={value => onChange(value)}
-            value={value}
-          />
-          
-        )}
-        
-        name="productName"
-        rules={{ required: true }}
-      />
-      {errors && (
-        <Text style={{color: 'red', alignSelf: 'stretch'}} >{errors.productName?.message}</Text>)}
-      
-      <Text style={styles.label}>Product Description</Text>
-      <Controller
-        control={control}
-        render={({field: { onChange, onBlur, value }}) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={value => onChange(value)}
-            value={value}
-          />
-        )}
-        name="productDescription"
-        rules={{ required: true }}
-      />
-       {errors && (
-        <Text style={{color: 'red', alignSelf: 'stretch'}} >{errors.productDescription?.message}</Text>)}
-        <Text style={styles.label}>Product Category</Text>
-      <Controller
-        control={control}
-        render={({field: { onChange, onBlur, value }}) => (
-          <TextInput
-            multiline={true}
-            numberOfLines={3}
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={value => onChange(value)}
-            value={value}
-          />
-        )}
-        name="productCategory"
-        rules={{ required: true }}
-      />
-       {errors && (
-        <Text style={{color: 'red', alignSelf: 'stretch'}} >{errors.productCategory?.message}</Text>)}
-      <Text style={styles.label}>Product Condition</Text>
-      <Controller
-        control={control}
-        render={({field: { onChange, onBlur, value }}) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={value => onChange(value)}
-            value={value}
-          />
-        )}
-        name="productCondition"
-        rules={{ required: true }}
-      />
-       {errors && (
-        <Text style={{color: 'red', alignSelf: 'stretch'}} >{errors.productCondition?.message}</Text>)}
-      <Text style={styles.label}>Listing Price</Text>
-      <Controller
-        control={control}
-        render={({field: { onChange, onBlur, value }}) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={value => onChange(value)}
-            value={value}
-          />
-        )}
-        name="productPrice"
-        rules={{ required: true }}
-      />
-       {errors && (
-        <Text style={{color: 'red', alignSelf: 'stretch'}} >{errors.productPrice?.message}</Text>)}
-      <Text style={styles.label}>City</Text>
-      <Controller
-        control={control}
-        render={({field: { onChange, onBlur, value }}) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={value => onChange(value)}
-            value={value}
-          />
-        )}
-        name="city"
-        rules={{ required: true }}
-      />
-       {errors && (
-        <Text style={{color: 'red', alignSelf: 'stretch'}} >{errors.city?.message}</Text>)}
-      <Text style={styles.label}>State</Text>
-      <Controller
-        control={control}
-        render={({field: { onChange, onBlur, value }}) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={value => onChange(value)}
-            value={value}
-          />
-        )}
-        name="state"
-        rules={{ required: true }}
-      />
-       {errors && (
-        <Text style={{color: 'red', alignSelf: 'stretch'}} >{errors.state?.message}</Text>)}
-      <View >
-        <CustomButton
-          text="Reset"
-          onPress={() => {
-            reset({
-              Name: '',
-              Description: ''
-            })
-          }}
-        />
-      </View>
+    <View style={{ paddingTop: StatusBar.currentHeight, }}>
+      <Header text={"Post Item"} />
 
-      <View >
-        <CustomButton
-          text="Post"
-          onPress={handleSubmit(onSubmit)}
-        />
-      </View>
-      </View>
-    </ScrollView>
+      <ScrollView style={{ paddingBottom: 550 }}>
+        <View style={styles.container}>
+
+          <TakePhoto setStateOfParent={setStateOfParent} />
+          <Text style={styles.label}>Product Name</Text>
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={value => onChange(value)}
+                value={value}
+              />
+
+            )}
+
+            name="productName"
+            rules={{ required: true }}
+          />
+          {errors && (
+            <Text style={{ color: 'red', alignSelf: 'stretch' }} >{errors.productName?.message}</Text>)}
+
+          <Text style={styles.label}>Product Description</Text>
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={value => onChange(value)}
+                value={value}
+              />
+            )}
+            name="productDescription"
+            rules={{ required: true }}
+          />
+          {errors && (
+            <Text style={{ color: 'red', alignSelf: 'stretch' }} >{errors.productDescription?.message}</Text>)}
+          <Text style={styles.label}>Product Category</Text>
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                multiline={true}
+                numberOfLines={3}
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={value => onChange(value)}
+                value={value}
+              />
+            )}
+            name="productCategory"
+            rules={{ required: true }}
+          />
+          {errors && (
+            <Text style={{ color: 'red', alignSelf: 'stretch' }} >{errors.productCategory?.message}</Text>)}
+          <Text style={styles.label}>Product Condition</Text>
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={value => onChange(value)}
+                value={value}
+              />
+            )}
+            name="productCondition"
+            rules={{ required: true }}
+          />
+          {errors && (
+            <Text style={{ color: 'red', alignSelf: 'stretch' }} >{errors.productCondition?.message}</Text>)}
+          <Text style={styles.label}>Listing Price</Text>
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={value => onChange(value)}
+                value={value}
+              />
+            )}
+            name="productPrice"
+            rules={{ required: true }}
+          />
+          {errors && (
+            <Text style={{ color: 'red', alignSelf: 'stretch' }} >{errors.productPrice?.message}</Text>)}
+          <Text style={styles.label}>City</Text>
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={value => onChange(value)}
+                value={value}
+              />
+            )}
+            name="city"
+            rules={{ required: true }}
+          />
+          {errors && (
+            <Text style={{ color: 'red', alignSelf: 'stretch' }} >{errors.city?.message}</Text>)}
+          <Text style={styles.label}>State</Text>
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={value => onChange(value)}
+                value={value}
+              />
+            )}
+            name="state"
+            rules={{ required: true }}
+          />
+          {errors && (
+            <Text style={{ color: 'red', alignSelf: 'stretch' }} >{errors.state?.message}</Text>)}
+          <View >
+            <CustomButton
+              text="Reset"
+              onPress={() => {
+                reset({
+                  Name: '',
+                  Description: ''
+                })
+              }}
+            />
+          </View>
+
+          <View >
+            <CustomButton
+              text="Post"
+              onPress={handleSubmit(onSubmit)}
+            />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -278,7 +305,7 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 4,
   },
-  
+
 });
 
 export default PostScreen;
